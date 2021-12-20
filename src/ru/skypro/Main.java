296d698d31e83c25b9c684c09855b20a50b10742
ved7997 <ved7997@gmail.com>
2021-12-20 23:07:46 +0300

    Сделанное задание от Алексея

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         String mir = "I'm coming to you";
-	System.out.println("Hello world! " + mir);// write your code here
+	System.out.println("Hello world!  " + mir);// write your code here
     }
 }
