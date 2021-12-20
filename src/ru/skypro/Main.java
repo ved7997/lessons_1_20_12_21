6a9bef96f942ee833e4ee45eaf4118a9b181451f
ved7997 <ved7997@gmail.com>
2021-12-20 22:57:19 +0300

    Мой первый Commit

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,7 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello world!");// write your code here
+        String mir = "I'm coming to you";
+	System.out.println("Hello world! " + mir);// write your code here
     }
 }
